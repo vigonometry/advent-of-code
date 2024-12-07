3ae33fcbced75e047f615dc61a8014efefc02297
Vigneshwar Hariharan <vigneshwar.hariharan1@gmail.com>
2024-12-07 19:36:56 +0800

    parallelize solution

--- aoc-2024/day-07/day_seven.py
+++ aoc-2024/day-07/day_seven.py
@@ -1,35 +1,32 @@
 import time
 from collections import deque
-# from math import floor, log10
+from multiprocessing import Pool
+from functools import partial
 
-f = [*open(0)]
-def soln(part):
-        res = 0
-        for line in f:
-            
-            target, nums = line.split(': ')
-            target = int(target)
-            
-            nums = list(map(int, nums.split()))
-            q = deque([(nums[0], 1)])
-            while len(q) > 0:
-                val, idx = q.popleft()
-                if idx == len(nums):
-                    if val == target:
-                        res += target
-                        break
-                    continue
-                q.append((val + nums[idx], idx + 1))
-                q.append((val * nums[idx], idx + 1))
-                if part:
-                    # q.append((val * 10**(1 + floor(log10(nums[idx]))) + nums[idx], idx + 1))
-                    q.append((int(f'{val}{nums[idx]}'), idx + 1))
-        return res
-
-t = time.time()
-print(f"Part 1: {soln(0)}, {round(time.time() - t, 3) * 1000}ms")
-t = time.time()
-print(f"Part 2: {soln(1)}, {round(time.time() - t, 3) * 1000}ms")
-        
-                
-                
+def solve(line, part):
+    target, nums = line.split(': ')
+    target = int(target)
+    
+    nums = list(map(int, nums.split()))
+    q = deque([(nums[0], 1)])
+    while len(q) > 0:
+        val, idx = q.popleft()
+        if idx == len(nums):
+            if val == target:
+                return target
+            continue
+        q.append((val + nums[idx], idx + 1))
+        q.append((val * nums[idx], idx + 1))
+        if part:
+            q.append((int(f'{val}{nums[idx]}'), idx + 1))
+    
+    return 0
+    
+if __name__ == "__main__":   
+    f = [*open(0)]  
+    with Pool() as pool:
+        t = time.time()
+        print(f"Part 1: {sum(pool.map(partial(solve, part=0), f))}, {round((time.time() - t) * 1000)}ms")
+        t = time.time()
+        print(f"Part 2: {sum(pool.map(partial(solve, part=1), f))}, {round((time.time() - t) * 1000)}ms")
+    pool.close()
